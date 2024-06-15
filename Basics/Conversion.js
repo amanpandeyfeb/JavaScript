8bf278659a043732478bd1184f638fb9e2d080ef
Aman Pandey <68867996+amanpandeyfeb@users.noreply.github.com>
2024-06-15 19:42:42 +0000

    again i have updated

--- Basics/Conversion.js
+++ Basics/Conversion.js
@@ -1,4 +1,4 @@
-let score =true
+/*let score =true
 
 console.log(typeof score);
 console.log(typeof(score));
@@ -6,3 +6,29 @@ console.log(typeof(score));
 let valueinNumber = Number(score)
 console.log(typeof valueinNumber);
 console.log(valueinNumber);
+*/
+
+
+//****************Operaion***************
+
+//console.log(2+2)
+
+let str1 = "hello"
+let str2 = "aman"
+
+let str3 = str1 +str2;
+//console.log(str3);
+
+
+//console.log("1"+2);//12
+//console.log(1+"2");//12
+//console.log("1"+2+2);//122
+//console.log(1+2+"4");//34
+
+//console.log("2">1);
+//console.log("aman">1);
+//console.log(null>=0);
+
+console.log("2"==2);//true
+console.log("2"===2);//false
+
